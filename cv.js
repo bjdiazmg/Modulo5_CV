8fed7c186a7794ac7068204f7288517cf9346ae6
bjdiazmg <diazbruuno@gmail.com>
2022-02-16 15:59:59 +0100

    20220216

--- cv.js
+++ cv.js
@@ -1,5 +1,31 @@
-function printHTML() {
-    if (window.print) {
-      window.print();
+function keyEnter(event) {
+    if (event.keyCode === 13) {
+        showresult();
+        console.log('enter')
     }
-  }
+}
+
+function showresult() {
+    document.getElementById('result').innerHTML = '<b> Mensaje enviado, muchas gracias.</b>';
+};
+
+// Read more- Read Less function 
+
+var more = document.getElementById("more");
+    var showMore = document.getElementById("dots");
+    var btnText = document.getElementById("Btn");
+
+function showFunction() {
+      
+    if (more.style.display === "none") {
+      more.style.display = "inline";
+      btnText.innerHTML = "Read less";
+      showMore.style.display = "none";
+    } else {
+      more.style.display = "none";
+      btnText.innerHTML = "Read more";
+      showMore.style.display = "inline";
+    }
+  };
+
+  
